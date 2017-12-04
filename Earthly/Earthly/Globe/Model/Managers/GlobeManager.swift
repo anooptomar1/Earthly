74de648ebe0483272320ef7bef5378fa54ab010f
Max Rogers <marfodub@gmail.com>
2017-12-04 08:51:06 -0600

    Add App Icon

---
+++ Earthly/Earthly/Globe/Model/Managers/GlobeManager.swift
@@ -0,0 +1,29 @@
+//
+//  GlobeManager.swift
+//  Earthly
+//
+//  Created by Max Rogers on 12/3/17.
+//  Copyright © 2017 Max Rogers. All rights reserved.
+//
+
+import Foundation
+
+class GlobeManager {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Initialization
+    
+    init() {
+        // TODO: - figure out what init will have
+    }
+    
+//    func retrieve(remoteTile tile: RemoteTiles) {
+//
+//    }
+    
+    func retrieve(localTile tile: LocalTiles) {
+        
+    }
+}
